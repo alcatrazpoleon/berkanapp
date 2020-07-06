656b80a35f5281e6c6f972f2341f2555d0e8e993
alcatrazpoleon <restrepomedia@gmail.com>
2020-07-06 06:47:08 -0500

    Subiendo proyecto

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is scream body",
+      createdAt: "2020-06-23T13:01:16.906Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
